2fb61d3fdafc2185b0d75696ac61bb288765ec73
PriyadharshanSaba <priyadharshan.97@gmail.com>
2019-07-14 22:56:30 +0530

    API pilot

--- DjangoRestAPI/pg.py
+++ DjangoRestAPI/pg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import psycopg2
 import csv
+import pandas as pd
 
 
 hostname = 'ec2-54-243-47-196.compute-1.amazonaws.com'
@@ -9,9 +10,20 @@ password = 'aed79670385686eadee6afe4d13efac16bf37cb196d32cf2dbf833d31037e37f'
 database = 'd5bt2r3nq1onrl'
 
 
+
+
 """
 
-f = open('id.csv', 'r')
+df = pd.read_csv('if.csv',sep='\t')
+df = df[['ABHY0065001', 'RTGS-HO', 'ABHYUDAYA BANK BLDG., B.NO.71, NEHRU NAGAR, KURLA (E), MUMBAI-400024', 'MUMBAI', 'GREATER MUMBAI', 'MAHARASHTRA', '60']]
+df = df.head(9000)
+df.to_csv('if1.csv',sep='\t',index=False)
+
+"""
+
+
+"""
+f = open('id1.csv', 'r')
 
 def doQuery( conn ) :
     cur = conn.cursor()
@@ -49,6 +61,7 @@ def doQuery( conn ) :
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname = database)
 doQuery( myConnection )
 myConnection.close()
+
 
 
 #
